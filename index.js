c99c2f49e2ed07edb684b4270f2b6700c15fafad
Sarah Thiery <sarahvthiery@gmail.com>
2021-01-09 18:18:56 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let thankYous = []
+    for (let i = 0; i < names.length; i++) {
+        thankYous.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYous
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num)
+        num --
+    }
+}
